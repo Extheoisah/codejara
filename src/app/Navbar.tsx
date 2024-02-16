cddb2a67d1910869accae7e5853a652d62e6b93a
Umolo Blessing Johnson <umoloblessing009@gmail.com>
2024-02-16 15:34:27 +0100

    Navbar

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React, { useState } from "react";
+import Image from "next/image";
+import LOGO from "./Images/LOGO.svg";
+import Hamburger from "./Images/Hamburger.svg";
+import Cancel from "./Images/Cancel.svg";
+
+export default function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleToggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    return (
+        <div className={`${showMenu ? "h-[500px]" : ""} border-[#303030] border-[1px] rounded-[40px] py-[20px] mt-[30px] bg-[#111111]`}>
+            <div className="flex justify-between px-[20px]">
+                <div>
+                    <Image src={LOGO} alt="LOGO"></Image>
+                </div>
+                <div>
+                    {showMenu ? (
+                        <Image src={Cancel} alt="Cancel" onClick={handleToggleMenu}></Image>
+                    ) : (
+                        <Image src={Hamburger} alt="Hamburger" onClick={handleToggleMenu}></Image>
+                    )}
+                </div>
+            </div>
+            {showMenu && (
+                <div className="mt-[40px]">
+                    <p className="px-[20px] py-[25px]">For Talents</p>
+                    <hr className="bg-[#494949]" />
+                    <p className="px-[20px] py-[30px]">About Us</p>
+                    <hr className="bg-[#494949]" />
+                    <p className="px-[20px] py-[30px]">Contact Us</p>
+                    <hr className="bg-[#494949]" />
+                </div>
+            )}
+        </div>
+    );
+}
